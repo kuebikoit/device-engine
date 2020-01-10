2b5a14750ba63dffdc1374286e5d5ff5d1e49c04
Karya Chhetri <karyachhetri@karya.local>
2020-01-10 09:02:13 -0800

    get/put/delete

--- src/main/java/com/kuebikoit/deviceengine/controller/DeviceController.java
+++ src/main/java/com/kuebikoit/deviceengine/controller/DeviceController.java
@@ -1,8 +1,10 @@
 package com.kuebikoit.deviceengine.controller;
 
+import com.kuebikoit.deviceengine.exception.DeviceNotFoundException;
 import com.kuebikoit.deviceengine.persistence.model.Device;
 import com.kuebikoit.deviceengine.persistence.repository.DeviceRepository;
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -37,5 +39,42 @@ public class DeviceController {
 
         return (List<Device>) deviceRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Device getDevice(@PathVariable Long id) {
+        log.info("Get by id endpoint invoked id={}", id);
+        String exceptionMessage = String.format("Device not found for id=%s", id);
+
+        return deviceRepository.findById(id)
+            .orElseThrow(() -> new DeviceNotFoundException(exceptionMessage));
+
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeDevice(@PathVariable Long id) {
+        log.info("Delete by id endpoint invoked id={}", id);
+        String exceptionMessage = String.format("Device not found for id=%s", id);
+
+        Device deviceToRemove = deviceRepository.findById(id)
+            .orElseThrow(() -> new DeviceNotFoundException(exceptionMessage));
+
+        deviceRepository.delete(deviceToRemove);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Device updateDevice(@RequestBody Device device, @PathVariable Long id) {
+        log.info("Delete by id endpoint invoked id={}", id);
+        String exceptionMessage = String.format("Device not found for id=%s", id);
+
+        Device deviceToUpdate = deviceRepository.findById(id)
+            .orElseThrow(() -> new DeviceNotFoundException(exceptionMessage));
+
+
+        return deviceRepository.save(deviceToUpdate);
+    }
+
+
 
 }
