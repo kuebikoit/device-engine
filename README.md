9d0c7e7b54b8b49da6ba88e9c34303791a619700
Karya Chhetri <karyachhetri@karya.local>
2020-01-10 23:03:14 -0800

    basic spring security with client_credentials

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 ## Running the app
 ```mvn spring-boot:run -Dspring-boot.run.profiles=<profile>```
 
+## Security
+```
+curl <client-id>:<client-secret>@<url>/oauth/token -dgrant_type=client_credentials -dscope=any
+curl <url>/oauth/check_token/?token=<TOKEN>
+```
+
 ## Built With
 * [Maven](https://maven.apache.org/)
 * [Jenkins](http://jenkins.kuebikoit.com:8080/)
--- src/main/java/com/kuebikoit/deviceengine/DeviceEngineApplication.java
+++ src/main/java/com/kuebikoit/deviceengine/DeviceEngineApplication.java
@@ -2,14 +2,17 @@ package com.kuebikoit.deviceengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
 public class DeviceEngineApplication {
 
 
 	public static void main(String[] args)
 	{
-
 		SpringApplication.run(DeviceEngineApplication.class, args);
 	}
 
