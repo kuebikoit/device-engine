77f9ee6ee1c72bc6da3d3ea097a4f23d9fdf6ea0
Karya Chhetri <karyachhetri@karya.local>
2020-01-29 21:09:13 -0800

    setting timezone to utc

--- src/main/java/com/kuebikoit/deviceengine/DeviceEngineApplication.java
+++ src/main/java/com/kuebikoit/deviceengine/DeviceEngineApplication.java
@@ -1,5 +1,8 @@
 package com.kuebikoit.deviceengine;
 
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -11,4 +14,10 @@ public class DeviceEngineApplication {
   public static void main(String[] args) {
     SpringApplication.run(DeviceEngineApplication.class, args);
   }
+
+  @PostConstruct
+  public void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
+  }
+
 }
